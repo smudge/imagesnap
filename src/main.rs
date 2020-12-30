2098d9b45124744b06d5a95e63bbdd4529f95e1d
smudge <nathan@ngriffith.com>
2020-12-30 14:24:08 -0500

    More structure of main, handling args

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,6 @@ extern crate getopts;
 use getopts::Options;
 use std::env::args;
 
-fn print_usage(program: &String, opts: Options) {
-    println!("{} v{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
-    println!("  {}\n", env!("CARGO_PKG_DESCRIPTION"));
-    let brief = format!("Usage:\n  {} [<options>] [OUTPUT.(jpg|png|tif)]", program);
-    print!("{}", opts.usage(&brief));
-}
-
 fn main() {
     let args: Vec<String> = args().collect();
 
@@ -18,5 +11,36 @@ fn main() {
     opts.optopt("d", "device", "Use specific capture device", "NAME");
     opts.optflag("h", "help", "This help message");
 
-    print_usage(&args[0], opts);
+    match opts.parse(&args[1..]) {
+        Ok(m) => handle_args(&args[0], opts, m),
+        Err(_) => print_usage(&args[0], opts),
+    };
+}
+
+fn print_usage(program: &String, opts: Options) {
+    println!("{} v{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
+    println!("  {}\n", env!("CARGO_PKG_DESCRIPTION"));
+    let brief = format!("Usage:\n  {} [<options>] [OUTPUT.(jpg|png|tif)]", program);
+    print!("{}", opts.usage(&brief));
+}
+
+fn handle_args(program: &String, opts: Options, matches: getopts::Matches) {
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
+        return;
+    }
+    if matches.opt_present("l") {
+        println!("PRINT DEVICES");
+        return;
+    }
+    let device = if matches.opt_present("d") {
+        matches.opt_str("d").unwrap()
+    } else {
+        "DEFAULT DEVICE".to_string()
+    };
+    let input = if matches.free.is_empty() {
+        "snapshot.jpg".to_string()
+    } else {
+        matches.free[0].clone()
+    };
 }
