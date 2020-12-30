10382ccfdd64799468b632e4dc74f0f82624f997
smudge <nathan@ngriffith.com>
2020-12-29 21:42:41 -0500

    Use getopts

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,22 @@
+extern crate getopts;
+
+use getopts::Options;
 use std::env::args;
 
-fn print_usage(program: &String) {
+fn print_usage(program: &String, opts: Options) {
     println!("{} v{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
     println!("  {}\n", env!("CARGO_PKG_DESCRIPTION"));
-    println!("usage:\n  {} [-h | --help] [<args>] [<output>]\n", program);
-    println!("args:");
-    println!("  --help | -h      This help message");
-    println!("  --list | -l      List available capture devices");
-    println!("  --device | -d    Use specific capture device");
+    let brief = format!("Usage:\n  {} [<options>] [OUTPUT.(jpg|png|tif)]", program);
+    print!("{}", opts.usage(&brief));
 }
 
 fn main() {
     let args: Vec<String> = args().collect();
 
-    print_usage(&args[0]);
+    let mut opts = Options::new();
+    opts.optflag("l", "list", "List available capture devices");
+    opts.optopt("d", "device", "Use specific capture device", "NAME");
+    opts.optflag("h", "help", "This help message");
+
+    print_usage(&args[0], opts);
 }
