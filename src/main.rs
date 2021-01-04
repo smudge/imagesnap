47e016d123737058a0951620df08cf02919f7db4
smudge <nathan@ngriffith.com>
2021-01-04 11:43:40 -0500

    Start to handle and print errors

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use getopts::Options;
 use imagesnap::Camera;
 use std::env::args;
 
-fn main() {
+fn main() -> Result<(), String> {
     let args: Vec<String> = args().collect();
 
     let mut opts = Options::new();
@@ -16,19 +16,19 @@ fn main() {
 
     match opts.parse(&args[1..]) {
         Ok(m) => handle_args(&args[0], opts, m),
-        Err(_) => print_usage(&args[0], opts, 1),
-    };
+        Err(_) => print_usage(&args[0], opts, err("Failed to parse args!")),
+    }
 }
 
-fn print_usage(program: &String, opts: Options, code: i32) {
+fn print_usage(program: &String, opts: Options, result: Result<(), String>) -> Result<(), String> {
     println!("{} v{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
     println!("  {}\n", env!("CARGO_PKG_DESCRIPTION"));
     print!("Usage:\n  {} [<options>] [OUTPUT.(jpg|png|tif)]", program);
-    print!("{}", opts.usage(""));
-    std::process::exit(code);
+    println!("{}", opts.usage(""));
+    result
 }
 
-fn handle_args(program: &String, opts: Options, matches: getopts::Matches) {
+fn handle_args(program: &String, opts: Options, matches: getopts::Matches) -> Result<(), String> {
     match (
         matches.free.get(0).map(|s| s.to_owned()),
         matches.free.get(1),
@@ -38,13 +38,25 @@ fn handle_args(program: &String, opts: Options, matches: getopts::Matches) {
         matches.opt_str("w").map(|s| s.parse()).transpose(),
         matches.opt_str("d"),
     ) {
-        (filename, None, false, false, verbose, Ok(warmup), device) => {
+        (maybe_file, None, false, false, verbose, Ok(warmup), device) => {
             Camera::new(device, verbose, warmup)
-                .snap(filename.unwrap_or("snapshot.jpg".to_string()))
+                .snap(maybe_file.unwrap_or("snapshot.jpg".to_string()))
         }
         (None, None, true, false, _, Ok(_), _) => Camera::list_devices(),
-        (None, None, false, true, _, Ok(_), _) => Ok(print_usage(&program, opts, 0)),
-        (_, _, _, _, _, _, _) => Ok(print_usage(&program, opts, 1)),
+        (None, None, false, true, _, Ok(_), _) => print_usage(&program, opts, ok()),
+        (_, None, false, false, _, Err(_), _) => {
+            print_usage(&program, opts, err("Failed to parse warmup!"))
+        }
+        (_, _, _, _, _, _, _) => {
+            print_usage(&program, opts, err("Invalid combination of arguments."))
+        }
     }
-    .unwrap();
+}
+
+fn ok() -> Result<(), String> {
+    Ok(())
+}
+
+fn err(value: &str) -> Result<(), String> {
+    Err(value.to_string())
 }
