dd04861272a1320ffc7729d633a9c2650ba23cb8
smudge <nathan@ngriffith.com>
2021-01-29 15:00:08 -0500

    Make use of lazy static for ARGS and OPTS (truly global concepts)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#[macro_use]
+extern crate lazy_static;
 extern crate anyhow;
 extern crate getopts;
 
@@ -6,20 +8,24 @@ use futures::executor::block_on;
 use getopts::Options;
 use imagesnap::{Camera, Device};
 use std::env;
+use std::sync::Mutex;
 
 static DEFAULT_FILE: &str = "snapshot.jpg";
 
-fn main() -> Result<()> {
-    let args: Vec<String> = env::args().collect();
+lazy_static! {
+    static ref ARGS: Vec<String> = env::args().collect();
+    static ref OPTS: Mutex<Options> = Mutex::new(Options::new());
+}
 
-    let mut opts = Options::new();
+fn main() -> Result<()> {
+    let mut opts = OPTS.lock().unwrap();
     opts.optflag("q", "quiet", "Do not output any text");
     opts.optopt("w", "warmup", "Warm up camera for x seconds [0-10]", "x.x");
     opts.optflag("l", "list", "List available capture devices");
     opts.optopt("d", "device", "Use device with QUERY in its name", "QUERY");
     opts.optflag("h", "help", "This help message");
 
-    let matches = opts.parse(&args[1..])?;
+    let matches = OPTS.lock().unwrap().parse(&ARGS[1..])?;
     match (
         matches.free.get(0).map(|s| s.as_str()),
         matches.free.get(1),
@@ -36,20 +42,20 @@ fn main() -> Result<()> {
             snap(maybe_file.unwrap_or(DEFAULT_FILE), verbose, warmup, device)
         }
         (None, None, true, false, _, Ok(None), Ok(None)) => list_devices(),
-        (None, None, false, true, _, Ok(None), Ok(None)) => Ok(print_usage(&args[0], &opts)),
+        (None, None, false, true, _, Ok(None), Ok(None)) => Ok(print_usage()),
         (_, None, false, false, _, Err(_), _) => Err(anyhow!("Failed to parse warmup!")),
         (_, None, false, false, _, Ok(_), Err(e)) => Err(anyhow!(e.to_string())),
         (_, _, _, _, _, _, _) => Err(anyhow!("Invalid combination of arguments.")),
     }
 }
 
-fn print_usage(program: &String, opts: &Options) {
+fn print_usage() {
     println!("{} v{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
     println!("  {}\n", env!("CARGO_PKG_DESCRIPTION"));
-    println!("Usage:\n  {} [<OPTIONS>] [FILENAME]\n", program);
+    println!("Usage:\n  {} [<OPTIONS>] [FILENAME]\n", &ARGS[0]);
     println!("FILENAME:");
     print!("  Defaults to '{}' (only JPG supported)", DEFAULT_FILE);
-    println!("{}", opts.usage(""));
+    println!("{}", OPTS.lock().unwrap().usage(""));
 }
 
 fn list_devices() -> Result<()> {
