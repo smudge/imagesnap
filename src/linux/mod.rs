04ece85a885a2ce16f041889e46c289088d6a2c7
Nathan G <nathan@ngriffith.com>
2025-07-19 16:57:41 -0400

    Fix Linux build

--- src/linux/mod.rs
+++ src/linux/mod.rs
@@ -24,16 +24,22 @@ impl Client {
         Ok(names)
     }
 
-    pub async fn capture<S: Into<String>>(device_name: S, filename: S, warmup: f32) -> Result<(), String> {
+    pub async fn capture<S: Into<String>>(
+        device_name: S,
+        filename: S,
+        warmup: f32,
+    ) -> Result<(), String> {
         let device_name = device_name.into();
         let filename = filename.into();
         let mut camera = rscam::new(&device_name).map_err(|e| e.to_string())?;
-        camera.start(&rscam::Config {
-            interval: (1, 30),
-            resolution: (640, 480),
-            format: b"MJPG",
-            ..Default::default()
-        }).map_err(|e| e.to_string())?;
+        camera
+            .start(&rscam::Config {
+                interval: (1, 30),
+                resolution: (640, 480),
+                format: b"MJPG",
+                ..Default::default()
+            })
+            .map_err(|e| e.to_string())?;
         std::thread::sleep(std::time::Duration::from_secs_f32(warmup));
         let frame = camera.capture().map_err(|e| e.to_string())?;
         let mut file = fs::File::create(&filename).map_err(|e| e.to_string())?;
@@ -41,4 +47,3 @@ impl Client {
         Ok(())
     }
 }
-
